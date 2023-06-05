5c37f8fb3578778c252747f6b103b6ab093c8e1f
Ajiniyaz <scorpionskilful@gmail.com>
2023-06-05 10:49:57 +0500

    twelfth

--- 12.js
+++ 12.js
@@ -3,3 +3,35 @@
 матрицы, превосходящих среднее арифметическое значение элементов
 матрицы. Принять n=4, m=5.
 */
+
+let array = [];
+
+let n = 5;  //Введите размерность матрицы: 
+let m = 4;  //Введите количество строк в матрице:
+let multiply = n*m
+
+for (let i = 0; i < n; i++) {
+  array.push([]);
+  for (let j = 0; j < m; j++) {
+    array[i][j] = Math.floor(Math.random() * 20); //матрица А[n,m]
+  }
+}
+array
+let sum = 0;
+for (let k = 0; k < array.length; k++) {
+  // sum += array[k].reduce((a,b)=>a+b)
+  sum += array[k].reduce((sum, current) => sum + current, 0);
+}
+sum
+let arvg = Math.floor(sum / multiply)
+arvg
+
+let largeNumbers = [];
+for (let m = 0; m < array.length; m++) {
+  for (let l = 0; l < array.length; l++) {
+    if (array[m][l]>arvg) {
+      largeNumbers.push(array[m][l])
+    }    
+  }  
+}
+console.log(`превосходящих среднее арифметическое значение элементов: ${largeNumbers}, среднее арифметическое: ${arvg}`);
